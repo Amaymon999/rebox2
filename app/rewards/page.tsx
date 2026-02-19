3110e1ca09bf8edf0c14d86778fb2175b87df324
Amaymon999 <135551980+Amaymon999@users.noreply.github.com>
2026-02-19 16:58:11 +0100

    Delete app directory

--- app/rewards/page.tsx
+++
@@ -1,9 +0,0 @@
-import { RewardsCatalog } from "@/components/rewards-catalog";
-
-export default function RewardsPage() {
-  return (
-    <main className="pt-4">
-      <RewardsCatalog />
-    </main>
-  );
-}
